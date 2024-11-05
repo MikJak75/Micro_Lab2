c83dc0ac6b27c57eb8d13691114103c984af0825
Mikolaj Jakowski <mikojakowski@gmail.com>
2024-11-05 17:56:15 -0500

    commented code

--- book_recommendation.py
+++ book_recommendation.py
@@ -13,12 +13,13 @@ import fasttext
 import wakepy
 import numpy as np
 import pprint
-from numpy import dot
-from numpy.linalg import norm
 
 
+# suppress fasttext warning message
+fasttext.FastText.eprint = lambda x: None
 
 
+# function that takes in string of text and returns list of normalized tokens
 def norm_token(text):
     # normalize string
     text = text.lower()
@@ -33,6 +34,9 @@ def norm_token(text):
     return word_list
 
 
+# Process a file that holds a csv of books, return a dataframe holding the 
+# books we will conisder and a list of lists containg the tokenized strings
+# corresponding to genres and description
 def process_books(filename):
     full_books = pd.read_csv(filename)
 
@@ -53,7 +57,7 @@ def process_books(filename):
     for x in range(len(combined_books)):
         combined_books_nt.append(norm_token(combined_books[x]))
         
-    #books is a dataframe and combined_books is a series
+    #books is a dataframe and combined_books is a list
     return books, combined_books_nt
 
 # take in a list of words and return an average wordvec of all the words
@@ -62,6 +66,8 @@ def average_wordvec(words, ft_model):
 
     return np.mean(wv_array, axis=0)
 
+# take in the tokens for each book and a  word vectorization model
+# find the avg word vec for each book averaged over each token in the book
 def avg_vectorize_books(combined_books_nt, ft_model):
     combined_books_avgvecs = []
     for token_list in combined_books_nt:
@@ -71,12 +77,13 @@ def avg_vectorize_books(combined_books_nt, ft_model):
     
     return np.array(combined_books_avgvecs)
 
-
+# function to calculate cosine similarity
 def cos_sim(a, b):
-    cos_sim = dot(a, b)/(norm(a)*norm(b))
+    cos_sim = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
     return cos_sim
 
 
+# calculate cos_similarities for a vector of word vectors and a target 
 def compare_cos_sim(target, wordvecs):
     similarities = np.array([cos_sim(target, wv) for wv in wordvecs])
 
@@ -93,20 +100,26 @@ def main():
 
     books, combined_books_nt = process_books('books_1.Best_Books_Ever.csv')
 
+    # load the fastext model from a file containing it
+    # in this case it will output a 100 dimensional word vector
     ft_model = fasttext.load_model('cc.en.100.bin')
 
-    target_description_avgvec = average_wordvec(target_description_nt, ft_model)
 
+
+    target_description_avgvec = average_wordvec(target_description_nt, ft_model)
     combined_books_avgvecs = avg_vectorize_books(combined_books_nt, ft_model)
 
-    similarities = compare_cos_sim(target_description_avgvec, combined_books_avgvecs)
 
+    # get similarities and find index corresponding to highest cosine similarity between input
+    # description and deescription + genres of books
+    similarities = compare_cos_sim(target_description_avgvec, combined_books_avgvecs)
     most_similar_index = np.argmax(similarities)
 
     similarity_score = similarities[most_similar_index]
     title = books.iloc[most_similar_index]['title']
 
 
+    # print similiarity score and title
     print(f'Similarity Score: {similarity_score}')
     print(title)
 
